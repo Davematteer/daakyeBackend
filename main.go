806e294899bdf203b3b10d75a680f4e7060ede2c
Davematteer <davematteer@gmail.com>
2025-02-12 22:14:30 +0000

    starting daakye medical

--- main.go
+++ main.go
@@ -23,10 +23,29 @@ func ussd_callback(w http.ResponseWriter, r *http.Request) {
 	phone_number := r.FormValue("phoneNumber")
 	text := r.FormValue("text")
 
-	_ = fmt.Sprint("%s,%s,%s", session_id, service_code, phone_number)
+	_ = fmt.Sprintf("%s,%s,%s", session_id, service_code, phone_number)
 
 	if len(text) == 0 {
-
+		w.Write([]byte("Nigga what do you wanna see\n1. Account details\n2. Make a deposit?\n3. Check insurance plans"))
+		return
+	} else {
+		switch text {
+		case "1":
+			w.Write([]byte("CON Select an account option\n1. Account number\n2. Account Balance"))
+			return
+		case "2":
+			w.Write([]byte(fmt.Sprintf("END Your Phone Number is %s", phone_number)))
+			return
+		case "1*1":
+			w.Write([]byte("END Your Account Number is ACC1001"))
+			return
+		case "1*2":
+			w.Write([]byte("END Your Balance is NGN 20,000"))
+			return
+		default:
+			w.Write([]byte("END Invalid input"))
+			return
+		}
 	}
 }
 
@@ -38,7 +57,7 @@ func main() {
 	}
 
 	fmt.Println("This is the ussd app")
-	//http.HandleFunc("/", ussd_callback)
+	http.HandleFunc("/", ussd_callback)
 	http.HandleFunc("/test", test)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
